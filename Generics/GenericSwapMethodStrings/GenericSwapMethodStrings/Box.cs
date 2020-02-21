46cfa85e437587318502586a7d40c46af53d32d6
Fill99 <aleksandarfilisyan@abv.bg>
2020-02-21 14:42:58 +0200

    Add generics matherials

---
+++ Generics/GenericSwapMethodStrings/GenericSwapMethodStrings/Box.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericSwapMethodStrings
+{
+    public class Box<T>
+    {
+        public   List<T> Box
+        {
+            get
+            {
+                return this.Box;
+            }
+            set(T box)
+            {
+
+        }
+    }
+}
---
+++ Generics/GenericSwapMethodStrings/GenericSwapMethodStrings/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GenericSwapMethodStrings
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var personInfo = Console.ReadLine().Split();
+            string fullName = $"{personInfo[0]}{personInfo[1]}";
+            string address = $"{personInfo[2] }";
+            var nameAndBeer = Console.ReadLine().Split();
+            string name = nameAndBeer[0];
+            int beerAmount = int.Parse(nameAndBeer[1]);
+            var thridInput = Console.ReadLine().Split();
+            var firstArgument = int.Parse(thridInput[0]);
+            var secondArgument = double.Parse(thridInput[1]);
+            Tuple<string, st
+        }
+    }
+}
